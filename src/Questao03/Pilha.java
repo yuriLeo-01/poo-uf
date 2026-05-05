953c567d0ec18076799147a6e01a21324497af13
Yuri <yurileo2005@gmail.com>
2026-05-05 17:09:30 -0300

    feat: implementation of Question03

---
+++ src/Questao03/Pilha.java
@@ -0,0 +1,34 @@
+package Questao03;
+
+import java.util.LinkedList;
+
+public class Pilha<X> {
+    private LinkedList<X> elementos = new LinkedList<>();
+    private static final int tamanhoMax = 10;
+
+    public boolean isEmpty() {
+        return elementos.isEmpty();
+    }
+
+    public boolean isFull() {
+        return elementos.size() == tamanhoMax;
+    }
+
+    public X pop() {
+        if (!isEmpty()) {
+            return elementos.removeLast();
+        } else {
+            System.out.println("A pilha está vazia.");
+            return null;
+        }
+    }
+
+    public void push(X elemento) {
+        if (!isFull()) {
+            elementos.addLast(elemento);
+        } else {
+            System.out.println("A pilha está cheia!");
+        }
+    }
+
+}
